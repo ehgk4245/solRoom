a159f0d161016cebd067fe9e588971a591e5223c
ehgk4245 <ehgkrk14@naver.com>
2024-05-03 21:55:35 +0900

    update

--- sol-spring/src/main/java/com/solRoom/solspring/domain/Member.java
+++ sol-spring/src/main/java/com/solRoom/solspring/domain/Member.java
@@ -1,54 +1,36 @@
 package com.solRoom.solspring.domain;
 
-import jakarta.persistence.Entity;
-import jakarta.persistence.Id;
+import jakarta.persistence.*;
+import lombok.*;
 
 @Entity
+@Getter
+@Setter
+@Builder
+@NoArgsConstructor
+@AllArgsConstructor
 public class Member {
-    @Id
+    @Id @GeneratedValue(strategy = GenerationType.IDENTITY)
     private Long id;
+
+    @Column(nullable = false, length = 50, unique = true)
     private String email;
+    @Column(length = 100)
     private String password;
+    @Column(nullable = false)
     private String name;
+
+    @Column(nullable = false)
     private String address;
 
-    public Long getId() {
-        return id;
-    }
+    private String confirmPassword;
 
-    public void setId(Long id) {
-        this.id = id;
-    }
+//    @Enumerated(EnumType.STRING)
+//    private GrantedAuthority role;
 
-    public String getEmail() {
-        return email;
-    }
 
-    public void setEmail(String email) {
-        this.email = email;
-    }
 
-    public String getPassword() {
-        return password;
-    }
 
-    public void setPassword(String password) {
-        this.password = password;
-    }
 
-    public String getName() {
-        return name;
-    }
 
-    public void setName(String name) {
-        this.name = name;
-    }
-
-    public String getAddress() {
-        return address;
-    }
-
-    public void setAddress(String address) {
-        this.address = address;
-    }
 }
