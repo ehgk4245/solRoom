600ececeea3c680d68aadc90ef7c95bc013f77a9
ehgk4245 <ehgkrk14@naver.com>
2024-05-03 21:36:47 +0900

    update

---
+++ sol-spring/src/test/java/com/solRoom/solspring/SolSpringApplicationTests.java
@@ -0,0 +1,13 @@
+package com.solRoom.solspring;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class SolSpringApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
